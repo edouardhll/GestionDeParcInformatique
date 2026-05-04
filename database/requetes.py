7e13d530568ec3ed9e6c5ed93c5e8eb214e2a00c
Edouard Hillion <edouardhll@gmail.com>
2026-05-04 14:57:33 +0200

    Update requetes.py

--- database/requetes.py
+++ database/requetes.py
@@ -1,8 +1,11 @@
 from connexion import connexion
 
-insert = (
-    "INSERT INTO equipements (type, marque, modele, num_serie, emplacement, responsable, date_achat)"
-    "VALUES (%s, %s, %s, %s, %s, %s, %s)"
-)
-data = ()
-cursor.execute(insert, data)
+def ajouter_equipement(typE, marque, modele, num_serie, emplacement, responsable, date_achat):
+    insert = (
+        "INSERT INTO equipements (type, marque, modele, num_serie, emplacement, responsable, date_achat) "
+        "VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    )
+    data = (typE, marque, modele, num_serie, emplacement, responsable, date_achat)
+    cursor = connexion.cursor()
+    cursor.execute(insert, data)
+    connexion.commit()
