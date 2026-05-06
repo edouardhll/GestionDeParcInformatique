1654c60ca61a6fd06588ec689e0c56d3d41c33e0
Edouard Hillion <edouardhll@gmail.com>
2026-05-06 23:09:35 +0200

    Update requetes.py

--- database/requetes.py
+++ database/requetes.py
@@ -1,50 +1,72 @@
 from connexion import connexion
 from datetime import date
-
+import mysql.connector as cpy
 cnx = None
 
+
 def ajouter_equipement(typE, marque, modele, num_serie, emplacement, responsable, date_achat):
-    insert = (
-        "INSERT INTO equipements (type, marque, modele, num_serie, emplacement, responsable, date_achat) "
-        "VALUES (%s, %s, %s, %s, %s, %s, %s)"
-    )
-    data = (typE, marque, modele, num_serie, emplacement, responsable, date_achat)
-    cursor = cnx.cursor()
-    cursor.execute(insert, data)
-    cnx.commit()
+
+    try:
+        insert = (
+            "INSERT INTO equipements (type, marque, modele, num_serie, emplacement, responsable, date_achat) "
+            "VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        )
+        data = (typE, marque, modele, num_serie, emplacement, responsable, date_achat)
+        cursor = cnx.cursor()
+        cursor.execute(insert, data)
+        cnx.commit()
+
+    except cpy.Error as err:
+            print("L'ajout a échoué: {}".format(err))
+
 
 def lire_equipement():
-    read = (
-        "SELECT * FROM equipements"
-    )
-    cursor = cnx.cursor()
-    cursor.execute(read)
-    return cursor
+
+    try:
+        read = (
+            "SELECT * FROM equipements"
+        )
+        cursor = cnx.cursor()
+        cursor.execute(read)
+        return cursor
+    
+    except cpy.Error as err:
+            print("La lecture a échoué: {}".format(err))
+
 
 def maj_equipement(id_equipement, typE, marque, modele, num_serie, emplacement, responsable, date_achat):
-    update = (
-         "UPDATE equipements SET type = %s, marque = %s, modele = %s, num_serie = %s, emplacement = %s, responsable = %s, date_achat = %s WHERE id = %s"
-    )
-    data = (typE, marque, modele, num_serie, emplacement, responsable, date_achat, id_equipement)
-    cursor = cnx.cursor()
-    cursor.execute(update, data)
-    cnx.commit()
+
+    try:
+        update = (
+            "UPDATE equipements SET type = %s, marque = %s, modele = %s, num_serie = %s, emplacement = %s, responsable = %s, date_achat = %s WHERE id = %s"
+        )
+        data = (typE, marque, modele, num_serie, emplacement, responsable, date_achat, id_equipement)
+        cursor = cnx.cursor()
+        cursor.execute(update, data)
+        cnx.commit()
+
+    except cpy.Error as err:
+        print("La mise à jour a échoué: {}".format(err))
 
 def delete_equipement(id_equipement):
-    delete = (
-         "DELETE FROM equipements WHERE id = %s"
-    )
-    data = (id_equipement,)
-    cursor = cnx.cursor()
-    cursor.execute(delete, data)
-    cnx.commit()
+
+    try:
+        delete = (
+            "DELETE FROM equipements WHERE id = %s"
+        )
+        data = (id_equipement,)
+        cursor = cnx.cursor()
+        cursor.execute(delete, data)
+        cnx.commit()
+
+    except cpy.Error as err:
+            print("La suppression a échoué: {}".format(err))
 
 cnx = connexion()
 
 #ajouter_equipement('clavier', 'auchan', 'clavier20', 1235  , 'ici', 'moa', date(2026, 6, 1))
 #maj_equipement(2, 'clavier', 'auchan', 'clavier20', '1235', 'ici', 'toa', date(2026, 6, 1))
 #delete_equipement(3)
-
-cursor =  lire_equipement()
-for line in cursor:
-        print(line)
+#cursor =  lire_equipement()
+#for line in cursor:
+#        print(line)
