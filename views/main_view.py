a7c17b0c6e656cda1430eb4a33040b91eccc583a
Edouard Hillion <edouardhll@gmail.com>
2026-05-26 17:47:32 +0200

    260526

--- views/main_view.py
+++ views/main_view.py
@@ -6,15 +6,21 @@ from database.requetes import ajouter_equipement, lire_equipement, delete_equipe
 from datetime import datetime
 from tkinter import messagebox
 
+
+
+
+
 #----------Définition des polices----------#
 h1p = "Manrope"
 h2p = "Inter"
-
 
 #----------Définition des couleurs----------#
 bgc = "#F2F3FF"
 h1c = "#131B2E"
 h2c = "#434655"
+
+
+
 
 
 #----------Création de l'app dans une fonction'----------#
@@ -27,6 +33,7 @@ def lancer_app():
     root.minsize(1280, 720) #donne une taille minimal à la fenêtre ne pouvant pas être réduite
     root.iconbitmap("./assets/logo.ico") #donne un logo à l'app
     root.config(background=bgc) #couleur de l'arrière plan de la fenêtre
+
 
 
     #----------Création du cadre ACCUEIL ----------#
@@ -50,7 +57,7 @@ def lancer_app():
 
 
 
-#----------Fonction de la fenêtre avec le tableau----------#
+#----------Fonction de la fenêtre de lecture----------#
 def entry(home, root):
     home.pack_forget() #on ferme la fenêtre d'accueil
 
@@ -92,14 +99,26 @@ def entry(home, root):
     button_frame.pack(side=BOTTOM)
 
     #----------Création du bouton AJOUTER----------#
-    add_button = Button(button_frame, text="Ajouter un nouvel équipement", font=(h2p, 15), bg=bgc, fg=h2c, activebackground=bgc, command=lambda: add_tk(bdd_page, root, home))#appelle la fonction pour la nouvelle fenêtre, on importe home et root pour pouvoir les reconnaitre
+    add_button = Button(button_frame, text="Ajouter un nouvel équipement", font=(h2p, 15), bg=bgc, fg=h2c, activebackground=bgc, command=lambda: add_tk(bdd_page, root, home,))#appelle la fonction pour la nouvelle fenêtre, on importe home et root pour pouvoir les reconnaitre
     add_button.pack(side = LEFT, pady=25, padx=25)
     #----------Création du bouton MODIFIER----------#
-    edit_button = Button(button_frame, text="Modifier un équipement", font=(h2p, 15), bg=bgc, fg=h2c, activebackground=bgc)#appelle la fonction pour la nouvelle fenêtre, on importe home et root pour pouvoir les reconnaitre
+    edit_button = Button(button_frame, text="Modifier un équipement", font=(h2p, 15), bg=bgc, fg=h2c, activebackground=bgc, command=lambda: edit_tk(bdd_page, root, home, table))#appelle la fonction pour la nouvelle fenêtre, on importe home et root pour pouvoir les reconnaitre
     edit_button.pack(side=LEFT, pady=25, padx=25)
     #----------Création du bouton EFFACER----------#
-    delete_button = Button(button_frame, text="Supprimer un équipement", font=(h2p, 15), bg=bgc, fg=h2c, activebackground=bgc)#appelle la fonction pour la nouvelle fenêtre, on importe home et root pour pouvoir les reconnaitre
+    delete_button = Button(button_frame, text="Supprimer un équipement", font=(h2p, 15), bg=bgc, fg=h2c, activebackground=bgc, command=lambda: delete_tk(home))#appelle la fonction pour la nouvelle fenêtre, on importe home et root pour pouvoir les reconnaitre
     delete_button.pack(side = RIGHT, pady=25, padx=25)
+    #----------Création de la fonction pour EFFACER----------#
+    def delete_tk(home):
+        try:
+            ligne = table.selection()
+            value = table.item(ligne, 'values')
+            id = value[0]
+            delete_equipement(id)
+            bdd_page.pack_forget()
+            entry(home, root)
+        except Exception as err:
+            messagebox.showerror("Error", str(err))
+
     #on a fini on pack le tout
     bdd_page.pack(expand=YES)
 
@@ -180,6 +199,106 @@ def add_tk(bdd_page, root, home):
 
 
     validation_button = Button(bottom, text="Valider le nouvel équipement", font=(h2p, 15), bg=bgc, fg=h2c, activebackground=bgc, command=input_data)
+    validation_button.pack(side=BOTTOM, pady=25, padx=25)
+
+    frame.pack(expand=YES)
+
+
+
+
+#----------Fonction de la fenêtre pour MODIFIER----------#
+
+
+
+def edit_tk(bdd_page, root, home, table):
+
+    bdd_page.pack_forget() #on ferme la fenêtre précédente
+
+    ligne = table.selection()
+    value = table.item(ligne, 'values')
+    id = value[0]
+
+
+
+
+
+    #----------Création du cadre principal----------#
+    frame = Frame(root, bg=(bgc))
+
+    #----------Création du cadre bottom----------#
+    bottom = Frame(frame, bg = (bgc))
+    bottom.pack(side=BOTTOM)
+    #----------Création du cadre left----------#
+    left = Frame(frame, bg = (bgc))
+    left.pack(side=LEFT)
+    #----------Création du cadre right----------#
+    right = Frame(frame, bg = (bgc))
+    right.pack(side=RIGHT)
+
+
+    #Tous les noms à gauche
+    label_title = Label(left, text='Matériel :', font=(h2p, 15), bg=bgc, fg=h2c)
+    label_title.pack(pady=5, padx=5,)
+
+    label_title = Label(left, text='Marque :', font=(h2p, 15), bg=bgc, fg=h2c)
+    label_title.pack(pady=5, padx=5)
+
+    label_title = Label(left, text='Modèle :', font=(h2p, 15), bg=bgc, fg=h2c)
+    label_title.pack(pady=5, padx=5)
+
+    label_title = Label(left, text='Numéro de série :', font=(h2p, 15), bg=bgc, fg=h2c)
+    label_title.pack(pady=5, padx=5)
+
+    label_title = Label(left, text='Emplacement :', font=(h2p, 15), bg=bgc, fg=h2c)
+    label_title.pack(pady=5, padx=5)
+
+    label_title = Label(left, text='Responsable :', font=(h2p, 15), bg=bgc, fg=h2c)
+    label_title.pack(pady=5, padx=5)
+
+    label_title = Label(left, text='Date achat :', font=(h2p, 15), bg=bgc, fg=h2c)
+    label_title.pack(pady=5, padx=5)
+
+
+    #Tous les inputs à droite
+    input_type = Entry(right, font=(h2p, 15), bg=bgc, fg=h2c, width=20)
+    input_type.insert(0, value[1])
+    input_type.pack(pady=5, padx=5)
+
+    input_marque = Entry(right, font=(h2p, 15), bg=bgc, fg=h2c, width=20)
+    input_marque.insert(0, value[2])
+    input_marque.pack(pady=5, padx=5)
+
+    input_modele = Entry(right, font=(h2p, 15), bg=bgc, fg=h2c, width=20)
+    input_modele.insert(0, value[3])
+    input_modele.pack(pady=5, padx=5)
+
+    input_num_serie = Entry(right, font=(h2p, 15), bg=bgc, fg=h2c, width=20)
+    input_num_serie.insert(0, value[4])
+    input_num_serie.pack(pady=5, padx=5)
+
+    input_emplacement = Entry(right, font=(h2p, 15), bg=bgc, fg=h2c, width=20)
+    input_emplacement.insert(0, value[5])
+    input_emplacement.pack(pady=5, padx=5)
+
+    input_responsable = Entry(right, font=(h2p, 15), bg=bgc, fg=h2c, width=20)
+    input_responsable.insert(0, value[6])
+    input_responsable.pack(pady=5, padx=5)
+
+    input_date_achat = Entry(right, font=(h2p, 15), bg=bgc, fg=h2c, width=20)
+    input_date_achat.insert(0, value[7])
+    input_date_achat.pack(pady=5, padx=5)
+
+    def input_data():
+
+        try:
+            data = input_type.get(), input_marque.get(), input_modele.get(), input_num_serie.get(), input_emplacement.get(), input_responsable.get(), datetime.strptime(input_date_achat.get(), "%d/%m/%Y")
+            maj_equipement(id, *data) #ajouter_equipement() attend 7 entrées, mettre une étoile permet de lui renvoyer la liste en séparant les éléments qui la contiennent.
+            frame.pack_forget()
+            entry(home, root)
+        except Exception as err:
+            messagebox.showerror("Error", str(err))
+
+    validation_button = Button(bottom, text="Valider Les modifications", font=(h2p, 15), bg=bgc, fg=h2c, activebackground=bgc, command=input_data)
     validation_button.pack(side=BOTTOM, pady=25, padx=25)
 
     frame.pack(expand=YES)
